eb6c42f9b10a2ae9f73f08cdc0c1883d96f7d7d6
Sara <105264543+SaraFedlu@users.noreply.github.com>
2022-08-11 13:06:33 +0300

    Update more_fun.c

--- more_fun.c
+++ more_fun.c
@@ -1,111 +1,159 @@
 #include "shell.h"
 
 /**
- * _strcpy - Copy Source To Destination Char
- * @dest:Destination
- * @src:Source
- * Return: Copy Of Char *
+ * builtin_exit - exit of the program with the status
+ * @data: struct for the program's data
+ * Return: zero if sucess, or other number if its declared in the arguments
  */
-char *_strcpy(char *dest, char *src)
+int builtin_exit(data_of_program *data)
 {
-int i;
+	int i;
 
-i = 0;
+	if (data->tokens[1] != NULL)
+	{/*if exists arg for exit, check if is a number*/
+		for (i = 0; data->tokens[1][i]; i++)
+			if ((data->tokens[1][i] < '0' || data->tokens[1][i] > '9')
+				&& data->tokens[1][i] != '+')
+			{/*if is not a number*/
+				errno = 2;
+				return (2);
+			}
+		errno = _atoi(data->tokens[1]);
+	}
+	free_all_data(data);
+	exit(errno);
+}
 
-while (src[i])
-{
-dest[i] = src[i];
-i++;
-}
-dest[i] = '\0';
-return (dest);
-}
 /**
- * _strcat - Concat Two String
- * @dest:First String
- * @src:Second String
- * Return:First String + Second String Char *
+ * builtin_cd - change the current directory
+ * @data: struct for the program's data
+ * Return: zero if sucess, or other number if its declared in the arguments
  */
-char *_strcat(char *dest, char *src)
+int builtin_cd(data_of_program *data)
 {
-char *s = dest;
+	char *dir_home = env_get_key("HOME", data), *dir_old = NULL;
+	char old_dir[128] = {0};
+	int error_code = 0;
 
-while (*dest != '\0')
-{
-dest++;
+	if (data->tokens[1])
+	{
+		if (str_compare(data->tokens[1], "-", 0))
+		{
+			dir_old = env_get_key("OLDPWD", data);
+			if (dir_old)
+				error_code = set_work_directory(data, dir_old);
+			_print(env_get_key("PWD", data));
+			_print("\n");
+
+			return (error_code);
+		}
+		else
+		{
+			return (set_work_directory(data, data->tokens[1]));
+		}
+	}
+	else
+	{
+		if (!dir_home)
+			dir_home = getcwd(old_dir, 128);
+
+		return (set_work_directory(data, dir_home));
+	}
+	return (0);
 }
 
-while (*src != '\0')
-{
-*dest = *src;
-dest++;
-src++;
-}
-
-*dest = '\0';
-return (s);
-}
 /**
- * _strchr - Locate Charactere In String
- * @s:String Search In
- * @c:Char To Search For
- * Return: Pointer To Char*
+ * set_work_directory - set the work directory
+ * @data: struct for the program's data
+ * @new_dir: path to be set as work directory
+ * Return: zero if sucess, or other number if its declared in the arguments
  */
-char *_strchr(char *s, char c)
+int set_work_directory(data_of_program *data, char *new_dir)
 {
+	char old_dir[128] = {0};
+	int err_code = 0;
 
-do {
-if (*s == c)
-{
-break;
-}
-} while (*s++);
+	getcwd(old_dir, 128);
 
-return (s);
+	if (!str_compare(old_dir, new_dir, 0))
+	{
+		err_code = chdir(new_dir);
+		if (err_code == -1)
+		{
+			errno = 2;
+			return (3);
+		}
+		env_set_key("PWD", new_dir, data);
+	}
+	env_set_key("OLDPWD", old_dir, data);
+	return (0);
 }
+
 /**
- * _strncmp - Compare Amount (n) Of Characters Of Two Strings.
- * @s1: A String.
- * @s2: A String.
- * @n: Amount Of Characters To Compare.
- * Return: 1 If The Strings Don't Match Otherwise 0
+ * builtin_help - shows the environment where the shell runs
+ * @data: struct for the program's data
+ * Return: zero if sucess, or other number if its declared in the arguments
  */
-int _strncmp(const char *s1, const char *s2, size_t n)
+int builtin_help(data_of_program *data)
 {
-size_t i;
+	int i, length = 0;
+	char *mensajes[6] = {NULL};
 
-if (s1 == NULL)
-	return (-1);
-for (i = 0; i < n && s2[i]; i++)
-{
-if (s1[i] != s2[i])
-{
-return (1);
+	mensajes[0] = HELP_MSG;
+
+	/* validate args */
+	if (data->tokens[1] == NULL)
+	{
+		_print(mensajes[0] + 6);
+		return (1);
+	}
+	if (data->tokens[2] != NULL)
+	{
+		errno = E2BIG;
+		perror(data->command_name);
+		return (5);
+	}
+	mensajes[1] = HELP_EXIT_MSG;
+	mensajes[2] = HELP_ENV_MSG;
+	mensajes[3] = HELP_SETENV_MSG;
+	mensajes[4] = HELP_UNSETENV_MSG;
+	mensajes[5] = HELP_CD_MSG;
+
+	for (i = 0; mensajes[i]; i++)
+	{
+		length = str_length(data->tokens[1]);
+		if (str_compare(data->tokens[1], mensajes[i], length))
+		{
+			_print(mensajes[i] + length + 1);
+			return (1);
+		}
+	}
+	/*if there is no match, print error and return -1 */
+	errno = EINVAL;
+	perror(data->command_name);
+	return (0);
 }
-}
-return (0);
-}
+
 /**
- * _strdup - Duplicate A String
- * @str:String
- * Return: Duplicate String Failed Null
+ * builtin_alias - add, remove or show aliases
+ * @data: struct for the program's data
+ * Return: zero if sucess, or other number if its declared in the arguments
  */
-char *_strdup(char *str)
+int builtin_alias(data_of_program *data)
 {
-size_t len, i;
-char *str2;
+	int i = 0;
 
-len = _strlen(str);
-str2 = malloc(sizeof(char) * (len + 1));
+	/* if there are no arguments, print all environment */
+	if (data->tokens[1] == NULL)
+		return (print_alias(data, NULL));
 
-if (!str2)
-{
-return (NULL);
-}
+	while (data->tokens[++i])
+	{/* if there are arguments, set or print each env variable*/
+		if (count_characters(data->tokens[i], "="))
+			set_alias(data->tokens[i], data);
+		else
+			print_alias(data, data->tokens[i]);
+	}
 
-for (i = 0; i <= len; i++)
-{
-str2[i] = str[i];
-}
-return (str2);
+	return (0);
 }
