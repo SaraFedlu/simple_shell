683a1d6822efe65532948518d1d90ea5a30716a1
Sara <105264543+SaraFedlu@users.noreply.github.com>
2022-08-11 13:01:57 +0300

    Update builtin.c

--- builtin.c
+++ builtin.c
@@ -1,152 +1,92 @@
 #include "shell.h"
 
 /**
- * exit_bul - Exit Statue Shell
- * @cmd: Parsed Command
- * @input: User Input
- * @argv:Program Name
- * @c:Excute Count
- * Return: Void (Exit Statue)
+ * builtin_env - shows the environment where the shell runs
+ * @data: struct for the program's data
+ * Return: zero if sucess, or other number if its declared in the arguments
  */
-void  exit_bul(char **cmd, char *input, char **argv, int c)
+int builtin_env(data_of_program *data)
 {
-int statue, i = 0;
+	int i;
+	char cpname[50] = {'\0'};
+	char *var_copy = NULL;
 
-if (cmd[1] == NULL)
-{
-free(input);
-free(cmd);
-exit(EXIT_SUCCESS);
+	/* if not arguments */
+	if (data->tokens[1] == NULL)
+		print_environ(data);
+	else
+	{
+		for (i = 0; data->tokens[1][i]; i++)
+		{/* checks if exists a char = */
+			if (data->tokens[1][i] == '=')
+			{/* checks if exists a var with the same name and change its value*/
+			/* temporally */
+				var_copy = str_duplicate(env_get_key(cpname, data));
+				if (var_copy != NULL)
+					env_set_key(cpname, data->tokens[1] + i + 1, data);
+
+				/* print the environ */
+				print_environ(data);
+				if (env_get_key(cpname, data) == NULL)
+				{/* print the variable if it does not exist in the environ */
+					_print(data->tokens[1]);
+					_print("\n");
+				}
+				else
+				{/* returns the old value of the var*/
+					env_set_key(cpname, var_copy, data);
+					free(var_copy);
+				}
+				return (0);
+			}
+			cpname[i] = data->tokens[1][i];
+		}
+		errno = 2;
+		perror(data->command_name);
+		errno = 127;
+	}
+	return (0);
 }
-while (cmd[1][i])
-{
-if (_isalpha(cmd[1][i++]) != 0)
-{
-_prerror(argv, c, cmd);
-break;
-}
-else
-{
-statue = _atoi(cmd[1]);
-free(input);
-free(cmd);
-exit(statue);
-}
-}
-}
+
 /**
- * change_dir - Change Dirctory
- * @cmd: Parsed Command
- * @er: Statue Last Command Excuted
- * Return: 0 Succes 1 Failed (For Old Pwd Always 0 Case No Old PWD)
+ * builtin_set_env - ..
+ * @data: struct for the program's data
+ * Return: zero if sucess, or other number if its declared in the arguments
  */
-int change_dir(char **cmd, __attribute__((unused))int er)
+int builtin_set_env(data_of_program *data)
 {
-int value = -1;
-char cwd[PATH_MAX];
+	/* validate args */
+	if (data->tokens[1] == NULL || data->tokens[2] == NULL)
+		return (0);
+	if (data->tokens[3] != NULL)
+	{
+		errno = E2BIG;
+		perror(data->command_name);
+		return (5);
+	}
 
-if (cmd[1] == NULL)
-	value = chdir(getenv("HOME"));
+	env_set_key(data->tokens[1], data->tokens[2], data);
 
-else if (_strcmp(cmd[1], "-") == 0)
-{
-value = chdir(getenv("OLDPWD"));
+	return (0);
 }
-else
-	value = chdir(cmd[1]);
 
-if (value == -1)
-{
-perror("hsh");
-return (-1);
-}
-else if (value != -1)
-{
-getcwd(cwd, sizeof(cwd));
-setenv("OLDPWD", getenv("PWD"), 1);
-setenv("PWD", cwd, 1);
-}
-return (0);
-}
 /**
- * dis_env - Display Enviroment Variable
- * @cmd:Parsed Command
- * @er:Statue of Last command Excuted
- * Return:Always 0
+ * builtin_unset_env - ..
+ * @data: struct for the program's data'
+ * Return: ..
  */
-int dis_env(__attribute__((unused)) char **cmd, __attribute__((unused)) int er)
+int builtin_unset_env(data_of_program *data)
 {
-size_t i;
-int len;
+	/* validate args */
+	if (data->tokens[1] == NULL)
+		return (0);
+	if (data->tokens[2] != NULL)
+	{
+		errno = E2BIG;
+		perror(data->command_name);
+		return (5);
+	}
+	env_remove_key(data->tokens[1], data);
 
-for (i = 0; environ[i] != NULL; i++)
-{
-len = _strlen(environ[i]);
-write(1, environ[i], len);
-write(STDOUT_FILENO, "\n", 1);
-}
-return (0);
-}
-/**
- * display_help - Displaying Help For Builtin
- * @cmd:Parsed Command
- * @er: Statue Of Last Command Excuted
- * Return: 0 Succes -1 Fail
- */
-int display_help(char **cmd, __attribute__((unused))int er)
-{
-int fd, fw, rd = 1;
-char c;
-
-fd = open(cmd[1], O_RDONLY);
-if (fd < 0)
-{
-perror("Error");
-return (0);
-}
-while (rd > 0)
-{
-rd = read(fd, &c, 1);
-fw = write(STDOUT_FILENO, &c, rd);
-if (fw < 0)
-{
-return (-1);
-}
-}
-_putchar('\n');
-return (0);
-}
-/**
- * echo_bul - Excute Echo Cases
- * @st:Statue Of Last Command Excuted
- * @cmd: Parsed Command
- * Return: Always 0 Or Excute Normal Echo
- */
-int echo_bul(char **cmd, int st)
-{
-char *path;
-unsigned int  pid = getppid();
-
-if (_strncmp(cmd[1], "$?", 2) == 0)
-{
-print_number_in(st);
-PRINTER("\n");
-}
-else if (_strncmp(cmd[1], "$$", 2) == 0)
-{
-print_number(pid);
-PRINTER("\n");
-
-}
-else if (_strncmp(cmd[1], "$PATH", 5) == 0)
-{
-path = _getenv("PATH");
-PRINTER(path);
-PRINTER("\n");
-free(path);
-}
-else
-	return (print_echo(cmd));
-
-return (1);
+	return (0);
 }
