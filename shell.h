8d952fa2baee05a69156d1f88d09fcb343c06750
Sara <105264543+SaraFedlu@users.noreply.github.com>
2022-08-11 13:28:47 +0300

    Update shell.h

--- shell.h
+++ shell.h
@@ -15,7 +15,7 @@
 
 /************* MACROS **************/
 
-#include "macros.h" /* for msg help and prompt */
+#include "helpers.h" /* for msg help and prompt */
 
 /************* STRUCTURES **************/
 
