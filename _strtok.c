73bbba5c1860722583f4a61aa23efc9f01391359
Sara <105264543+SaraFedlu@users.noreply.github.com>
2022-08-11 12:59:58 +0300

    edit _strtok

--- _strtok.c
+++ _strtok.c
@@ -1,66 +1,42 @@
 #include "shell.h"
 /**
- * check_delim - Checks If A Character Match Any Char
- * @c: Character To Check
- * @str: String To Check
- * Return: 1 Succes, 0 Failed
- */
-unsigned int check_delim(char c, const char *str)
+ * _strtok - separates strings with delimiters
+ * @line: It´s pointer to array we receive in getline.
+ * @delim: It´s characters we mark off string in parts.
+ * Return: A pointer to the created token
+*/
+char *_strtok(char *line, char *delim)
 {
-unsigned int i;
+	int j;
+	static char *str;
+	char *copystr;
 
-for (i = 0; str[i] != '\0'; i++)
-{
-if (c == str[i])
-	return (1);
-
-}
-return (0);
-}
-
-/**
- * _strtok - Token A String Into Token (strtrok)
- * @str: String
- * @delim: Delimiter
- * Return: Pointer To The Next Token Or NULL
- */
-char *_strtok(char *str, const char *delim)
-{
-static char *ts;
-static char *nt;
-unsigned int i;
-
-if (str != NULL)
-	nt = str;
-ts = nt;
-if (ts == NULL)
-	return (NULL);
-
-for (i = 0; ts[i] != '\0'; i++)
-{
-if (check_delim(ts[i], delim) == 0)
-	break;
-}
-if (nt[i] == '\0' || nt[i] == '#')
-{
-nt = NULL;
-return (NULL);
-}
-ts = nt + i;
-nt = ts;
-for (i = 0; nt[i] != '\0'; i++)
-{
-if (check_delim(nt[i], delim) == 1)
-	break;
-}
-if (nt[i] == '\0')
-	nt = NULL;
-else
-{
-nt[i] = '\0';
-nt = nt + i + 1;
-if (*nt == '\0')
-	nt = NULL;
-}
-return (ts);
+	if (line != NULL)
+		str = line;
+	for (; *str != '\0'; str++)
+	{
+		for (j = 0; delim[j] != '\0'; j++)
+		{
+			if (*str == delim[j])
+			break;
+		}
+		if (delim[j] == '\0')
+			break;
+	}
+	copystr = str;
+	if (*copystr == '\0')
+		return (NULL);
+	for (; *str != '\0'; str++)
+	{
+		for (j = 0; delim[j] != '\0'; j++)
+		{
+			if (*str == delim[j])
+			{
+				*str = '\0';
+				str++;
+				return (copystr);
+			}
+		}
+	}
+	return (copystr);
 }
