9764b026482509e5bf7e3cb577caa8903551920e
Sara <105264543+SaraFedlu@users.noreply.github.com>
2022-08-11 13:10:20 +0300

    Update finding_path.c

--- finding_path.c
+++ finding_path.c
@@ -1,96 +1,120 @@
 #include "shell.h"
 
+int check_file(char *full_path);
+
 /**
- * path_cmd -  Search In $PATH For Excutable Command
- * @cmd: Parsed Input
- * Return: 1  Failure  0  Success.
+ * find_program - find a program in path
+ * @data: a pointer to the program's data
+ * Return: 0 if success, errcode otherwise
  */
-int path_cmd(char **cmd)
+int find_program(data_of_program *data)
 {
-char *path, *value, *cmd_path;
-struct stat buf;
+	int i = 0, ret_code = 0;
+	char **directories;
 
-path = _getenv("PATH");
-value = _strtok(path, ":");
-while (value != NULL)
-{
-cmd_path = build(*cmd, value);
+	if (!data->command_name)
+		return (2);
 
-if (stat(cmd_path, &buf) == 0)
-{
-*cmd = _strdup(cmd_path);
-free(cmd_path);
-free(path);
-return (0);
+	/**if is a full_path or an executable in the same path */
+	if (data->command_name[0] == '/' || data->command_name[0] == '.')
+		return (check_file(data->command_name));
 
+	free(data->tokens[0]);
+	data->tokens[0] = str_concat(str_duplicate("/"), data->command_name);
+	if (!data->tokens[0])
+		return (2);
+
+	directories = tokenize_path(data);/* search in the PATH */
+
+	if (!directories || !directories[0])
+	{
+		errno = 127;
+		return (127);
+	}
+	for (i = 0; directories[i]; i++)
+	{/* appends the function_name to path */
+		directories[i] = str_concat(directories[i], data->tokens[0]);
+		ret_code = check_file(directories[i]);
+		if (ret_code == 0 || ret_code == 126)
+		{/* the file was found, is not a directory and has execute permisions*/
+			errno = 0;
+			free(data->tokens[0]);
+			data->tokens[0] = str_duplicate(directories[i]);
+			free_array_of_pointers(directories);
+			return (ret_code);
+		}
+	}
+	free(data->tokens[0]);
+	data->tokens[0] = NULL;
+	free_array_of_pointers(directories);
+	return (ret_code);
 }
-free(cmd_path);
-value = _strtok(NULL, ":");
-}
-free(path);
 
-return (1);
-}
 /**
- * build - Build Command
- * @token: Excutable Command
- * @value: Dirctory Conatining Command
- * Return: Parsed Full Path Of Command Or NULL Case Failed
+ * tokenize_path - tokenize the path in directories
+ * @data: a pointer to the program's data
+ * Return: array of path directories
  */
-char *build(char *token, char *value)
+char **tokenize_path(data_of_program *data)
 {
-char *cmd;
-size_t len;
+	int i = 0;
+	int counter_directories = 2;
+	char **tokens = NULL;
+	char *PATH;
 
-len = _strlen(value) + _strlen(token) + 2;
-cmd = malloc(sizeof(char) * len);
-if (cmd == NULL)
-{
-return (NULL);
+	/* get the PATH value*/
+	PATH = env_get_key("PATH", data);
+	if ((PATH == NULL) || PATH[0] == '\0')
+	{/*path not found*/
+		return (NULL);
+	}
+
+	PATH = str_duplicate(PATH);
+
+	/* find the number of directories in the PATH */
+	for (i = 0; PATH[i]; i++)
+	{
+		if (PATH[i] == ':')
+			counter_directories++;
+	}
+
+	/* reserve space for the array of pointers */
+	tokens = malloc(sizeof(char *) * counter_directories);
+
+	/*tokenize and duplicate each token of path*/
+	i = 0;
+	tokens[i] = str_duplicate(_strtok(PATH, ":"));
+	while (tokens[i++])
+	{
+		tokens[i] = str_duplicate(_strtok(NULL, ":"));
+	}
+
+	free(PATH);
+	PATH = NULL;
+	return (tokens);
+
 }
-memset(cmd, 0, len);
 
-cmd = _strcat(cmd, value);
-cmd = _strcat(cmd, "/");
-cmd = _strcat(cmd, token);
-
-return (cmd);
-}
 /**
- * _getenv - Gets The Value Of Enviroment Variable By Name
- * @name: Environment Variable Name
- * Return: The Value of the Environment Variable Else NULL.
+ * check_file - checks if exists a file, if it is not a dairectory and
+ * if it has excecution permisions for permisions.
+ * @full_path: pointer to the full file name
+ * Return: 0 on success, or error code if it exists.
  */
-char *_getenv(char *name)
+int check_file(char *full_path)
 {
-size_t nl, vl;
-char *value;
-int i, x, j;
+	struct stat sb;
 
-nl = _strlen(name);
-
-for (i = 0 ; environ[i]; i++)
-{
-if (_strncmp(name, environ[i], nl) == 0)
-{
-vl = _strlen(environ[i]) - nl;
-value = malloc(sizeof(char) * vl);
-
-if (!value)
-{
-free(value);
-perror("unable to alloc");
-return (NULL);
-}
-j = 0;
-for (x = nl + 1; environ[i][x]; x++, j++)
-{
-value[j] = environ[i][x];
-}
-value[j] = '\0';
-
-return (value);
-}
-}
-return (NULL);
+	if (stat(full_path, &sb) != -1)
+	{
+		if (S_ISDIR(sb.st_mode) ||  access(full_path, X_OK))
+		{
+			errno = 126;
+			return (126);
+		}
+		return (0);
+	}
+	/*if not exist the file*/
+	errno = 127;
+	return (127);
 }
