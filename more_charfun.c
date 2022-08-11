3353578f4afdd6dfd5f43f0d26857a08bbc89321
Sara <105264543+SaraFedlu@users.noreply.github.com>
2022-08-11 13:21:38 +0300

    Update more_charfun.c

--- more_charfun.c
+++ more_charfun.c
@@ -1,109 +1,41 @@
 #include "shell.h"
-
 /**
- * _strcmp - Compare Two String
- * @s1:String 1
- * @s2:String 2
- * Return: 0 If Identical Otherwise How Much Diffrent
+ * tokenize - this function separate the string using a designed delimiter
+ * @data: a pointer to the program's data
+ * Return: an array of the different parts of the string
  */
-int _strcmp(char *s1, char *s2)
+void tokenize(data_of_program *data)
 {
-int cmp = 0, i, len1, len2;
+	char *delimiter = " \t";
+	int i, j, counter = 2, length;
 
-len1 = _strlen(s1);
-len2 = _strlen(s2);
+	length = str_length(data->input_line);
+	if (length)
+	{
+		if (data->input_line[length - 1] == '\n')
+			data->input_line[length - 1] = '\0';
+	}
 
-if (s1 == NULL || s2 == NULL)
-	return (1);
+	for (i = 0; data->input_line[i]; i++)
+	{
+		for (j = 0; delimiter[j]; j++)
+		{
+			if (data->input_line[i] == delimiter[j])
+				counter++;
+		}
+	}
 
-if (len1 != len2)
-	return (1);
-
-for (i = 0; s1[i]; i++)
-{
-if (s1[i] != s2[i])
-{
-cmp = s1[i] - s2[i];
-break;
-}
-else
-	continue;
-}
-return (cmp);
-}
-/**
- * _isalpha - Check if Alphabtic
- * @c: Character
- * Return: 1 If True 0 If Not
- */
-int _isalpha(int c)
-{
-if (((c >= 97) && (c <= 122)) || ((c >= 65) && (c <= 90)))
-{
-return (1);
-}
-else
-{
-return (0);
-}
-}
-/**
- * _itoa - Convert Integer To Char
- * @n: Int To Convert
- * Return: Char Pointer
- */
-char *_itoa(unsigned int n)
-{
-int len = 0, i = 0;
-char *s;
-
-len = intlen(n);
-s = malloc(len + 1);
-if (!s)
-	return (NULL);
-*s = '\0';
-while (n / 10)
-{
-s[i] = (n % 10) + '0';
-n /= 10;
-i++;
-}
-s[i] = (n % 10) + '0';
-array_rev(s, len);
-s[i + 1] = '\0';
-return (s);
-}
-/**
- *  array_rev - Reverse Array
- *  @arr:Array To Reverse
- *  @len:Length Of Array
- *  Return: Void(Reverse Array)
- */
-void array_rev(char *arr, int len)
-{
-int i;
-char tmp;
-
-for (i = 0; i < (len / 2); i++)
-{
-tmp = arr[i];
-arr[i] = arr[(len - 1) - i];
-arr[(len - 1) - i] = tmp;
-}
-}
-/**
- * intlen - Determine Length Of Int
- * @num: Given Int
- * Return: Length Of Int
- */
-int intlen(int num)
-{
-int len = 0;
-
-while (num != 0)
-{
-len++;
-num /= 10;
-}
-return (len);
+	data->tokens = malloc(counter * sizeof(char *));
+	if (data->tokens == NULL)
+	{
+		perror(data->program_name);
+		exit(errno);
+	}
+	i = 0;
+	data->tokens[i] = str_duplicate(_strtok(data->input_line, delimiter));
+	data->command_name = str_duplicate(data->tokens[0]);
+	while (data->tokens[i++])
+	{
+		data->tokens[i] = str_duplicate(_strtok(NULL, delimiter));
+	}
 }
