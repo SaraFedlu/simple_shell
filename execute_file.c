3947fa0721a97560610e643f907ac7323f8d1cd5
Sara <105264543+SaraFedlu@users.noreply.github.com>
2022-08-11 13:14:29 +0300

    Update execute_file.c

--- execute_file.c
+++ execute_file.c
@@ -1,92 +1,112 @@
 #include "shell.h"
 
 /**
- * read_file - Read Command From File
- * @filename:Filename
- * @argv:Program Name
- * Return: -1 or  0
+ * expand_variables - expand variables
+ * @data: a pointer to a struct of the program's data
+ *
+ * Return: nothing, but sets errno.
  */
-void read_file(char *filename, char **argv)
+void expand_variables(data_of_program *data)
 {
-FILE *fp;
-char *line = NULL;
-size_t len = 0;
-int counter = 0;
+	int i, j;
+	char line[BUFFER_SIZE] = {0}, expansion[BUFFER_SIZE] = {'\0'}, *temp;
 
-fp = fopen(filename, "r");
-if (fp == NULL)
-{
-exit(EXIT_FAILURE);
+	if (data->input_line == NULL)
+		return;
+	buffer_add(line, data->input_line);
+	for (i = 0; line[i]; i++)
+		if (line[i] == '#')
+			line[i--] = '\0';
+		else if (line[i] == '$' && line[i + 1] == '?')
+		{
+			line[i] = '\0';
+			long_to_string(errno, expansion, 10);
+			buffer_add(line, expansion);
+			buffer_add(line, data->input_line + i + 2);
+		}
+		else if (line[i] == '$' && line[i + 1] == '$')
+		{
+			line[i] = '\0';
+			long_to_string(getpid(), expansion, 10);
+			buffer_add(line, expansion);
+			buffer_add(line, data->input_line + i + 2);
+		}
+		else if (line[i] == '$' && (line[i + 1] == ' ' || line[i + 1] == '\0'))
+			continue;
+		else if (line[i] == '$')
+		{
+			for (j = 1; line[i + j] && line[i + j] != ' '; j++)
+				expansion[j - 1] = line[i + j];
+			temp = env_get_key(expansion, data);
+			line[i] = '\0', expansion[0] = '\0';
+			buffer_add(expansion, line + i + j);
+			temp ? buffer_add(line, temp) : 1;
+			buffer_add(line, expansion);
+		}
+	if (!str_compare(data->input_line, line, 0))
+	{
+		free(data->input_line);
+		data->input_line = str_duplicate(line);
+	}
 }
-while ((getline(&line, &len, fp)) != -1)
-{
-counter++;
-treat_file(line, counter, fp, argv);
 
-}
-if (line)
-	free(line);
-fclose(fp);
-exit(0);
-}
 /**
- * treat_file - PARSE Check Command Fork Wait Excute in Line of File
- * @line: Line From A File
- * @counter:Error Counter
- * @fp:File Descriptor
- * @argv:Program Name
- * Return : Excute A line void
+ * expand_alias - expans aliases
+ * @data: a pointer to a struct of the program's data
+ *
+ * Return: nothing, but sets errno.
  */
-void treat_file(char *line, int counter, FILE *fp, char **argv)
+void expand_alias(data_of_program *data)
 {
-char **cmd;
-int st = 0;
+	int i, j, was_expanded = 0;
+	char line[BUFFER_SIZE] = {0}, expansion[BUFFER_SIZE] = {'\0'}, *temp;
 
-cmd = parse_cmd(line);
+	if (data->input_line == NULL)
+		return;
 
-if (_strncmp(cmd[0], "exit", 4) == 0)
-{
-exit_bul_for_file(cmd, line, fp);
+	buffer_add(line, data->input_line);
+
+	for (i = 0; line[i]; i++)
+	{
+		for (j = 0; line[i + j] && line[i + j] != ' '; j++)
+			expansion[j] = line[i + j];
+		expansion[j] = '\0';
+
+		temp = get_alias(data, expansion);
+		if (temp)
+		{
+			expansion[0] = '\0';
+			buffer_add(expansion, line + i + j);
+			line[i] = '\0';
+			buffer_add(line, temp);
+			line[str_length(line)] = '\0';
+			buffer_add(line, expansion);
+			was_expanded = 1;
+		}
+		break;
+	}
+	if (was_expanded)
+	{
+		free(data->input_line);
+		data->input_line = str_duplicate(line);
+	}
 }
-else if (check_builtin(cmd) == 0)
-{
-st = handle_builtin(cmd, st);
-free(cmd);
-}
-else
-{
-st = check_cmd(cmd, line, counter, argv);
-free(cmd);
-}
-}
+
 /**
- * exit_bul_for_file - Exit Shell Case Of File
- * @line: Line From A File
- * @cmd: Parsed Command
- * @fd:File Descriptor
- * Return : Case Of Exit in A File Line
+ * buffer_add - append string at end of the buffer
+ * @buffer: buffer to be filled
+ * @str_to_add: string to be copied in the buffer
+ * Return: nothing, but sets errno.
  */
-void exit_bul_for_file(char **cmd, char *line, FILE *fd)
+int buffer_add(char *buffer, char *str_to_add)
 {
-int statue, i = 0;
+	int length, i;
 
-if (cmd[1] == NULL)
-{
-free(line);
-free(cmd);
-fclose(fd);
-exit(errno);
-}
-while (cmd[1][i])
-{
-if (_isalpha(cmd[1][i++]) < 0)
-{
-perror("illegal number");
-}
-}
-statue = _atoi(cmd[1]);
-free(line);
-free(cmd);
-fclose(fd);
-exit(statue);
+	length = str_length(buffer);
+	for (i = 0; str_to_add[i]; i++)
+	{
+		buffer[length + i] = str_to_add[i];
+	}
+	buffer[length + i] = '\0';
+	return (length + i);
 }
