866b7b1852e7f261f9ec3b609f2775c009eb5422
Sara <105264543+SaraFedlu@users.noreply.github.com>
2022-08-11 12:55:36 +0300

    Update shell.c

--- shell.c
+++ shell.c
@@ -1,94 +1,120 @@
 #include "shell.h"
-
 /**
- * main - Simple Shell (Hsh)
- * @argc: Argument Count
- * @argv:Argument Value
- * Return: Exit Value By Status
+ * main - initialize the variables of the program
+ * @argc: number of values received from the command line
+ * @argv: values received from the command line
+ * @env: number of values received from the command line
+ * Return: zero on succes.
  */
-int main(__attribute__((unused)) int argc, char **argv)
+int main(int argc, char *argv[], char *env[])
 {
-char *input, **cmd;
-int counter = 0, statue = 1, st = 0;
+	data_of_program data_struct = {NULL}, *data = &data_struct;
+	char *prompt = "";
 
-if (argv[1] != NULL)
-	read_file(argv[1], argv);
+	initialize_data(data, argc, argv, env);
 
-signal(SIGINT, signal_to_handel);
-while (statue)
-{
-counter++;
-if (isatty(STDIN_FILENO))
-	prompt();
+	signal(SIGINT, handle_ctrl_c);
 
-input = _getline();
-
-if (input[0] == '\0')
-{
-continue;
-}
-history(input);
-cmd = parse_cmd(input);
-if (_strcmp(cmd[0], "exit") == 0)
-{
-exit_bul(cmd, input, argv, counter);
-}
-else if (check_builtin(cmd) == 0)
-{
-st = handle_builtin(cmd, st);
-free_all(cmd, input);
-continue;
-}
-else
-{
-st = check_cmd(cmd, input, counter, argv);
-
-}
-free_all(cmd, input);
-}
-return (statue);
-}
-/**
- * check_builtin - check builtin
- * @cmd:command to check
- * Return: 0 Succes -1 Fail
- */
-int check_builtin(char **cmd)
-{
-bul_t fun[] = {
-	{"cd", NULL},
-	{"help", NULL},
-	{"echo", NULL},
-	{"history", NULL},
-	{NULL, NULL}
-};
-
-int i = 0;
-
-if (*cmd == NULL)
-{
-return (-1);
-}
-
-while ((fun + i)->command)
-{
-if (_strcmp(cmd[0], (fun + i)->command) == 0)
+	if (isatty(STDIN_FILENO) && isatty(STDOUT_FILENO) && argc == 1)
+	{/* We are in the terminal, interactive mode */
+		errno = 2;/*???????*/
+		prompt = PROMPT_MSG;
+	}
+	errno = 0;
+	sisifo(prompt, data);
 	return (0);
-i++;
 }
-return (-1);
+
+/**
+ * handle_ctrl_c - print the prompt in a new line
+ * when the signal SIGINT (ctrl + c) is send to the program
+ * @UNUSED: option of the prototype
+ */
+void handle_ctrl_c(int opr UNUSED)
+{
+	_print("\n");
+	_print(PROMPT_MSG);
+}
+
+/**
+ * initialize_data - inicialize the struct with the info of the program
+ * @data: pointer to the structure of data
+ * @argv: array of arguments pased to the program execution
+ * @env: environ pased to the program execution
+ * @argc: number of values received from the command line
+ */
+void initialize_data(data_of_program *data, int argc, char *argv[], char **env)
+{
+	int i = 0;
+
+	data->program_name = argv[0];
+	data->input_line = NULL;
+	data->command_name = NULL;
+	data->exec_counter = 0;
+	/* define the file descriptor to be readed*/
+	if (argc == 1)
+		data->file_descriptor = STDIN_FILENO;
+	else
+	{
+		data->file_descriptor = open(argv[1], O_RDONLY);
+		if (data->file_descriptor == -1)
+		{
+			_printe(data->program_name);
+			_printe(": 0: Can't open ");
+			_printe(argv[1]);
+			_printe("\n");
+			exit(127);
+		}
+	}
+	data->tokens = NULL;
+	data->env = malloc(sizeof(char *) * 50);
+	if (env)
+	{
+		for (; env[i]; i++)
+		{
+			data->env[i] = str_duplicate(env[i]);
+		}
+	}
+	data->env[i] = NULL;
+	env = data->env;
+
+	data->alias_list = malloc(sizeof(char *) * 20);
+	for (i = 0; i < 20; i++)
+	{
+		data->alias_list[i] = NULL;
+	}
 }
 /**
- * creat_envi - Creat Array of Enviroment Variable
- * @envi: Array of Enviroment Variable
- * Return: Void
+ * sisifo - its a infinite loop that shows the prompt
+ * @prompt: prompt to be printed
+ * @data: its a infinite loop that shows the prompt
  */
-void creat_envi(char **envi)
+void sisifo(char *prompt, data_of_program *data)
 {
-int i;
+	int error_code = 0, string_len = 0;
 
-for (i = 0; environ[i]; i++)
-	envi[i] = _strdup(environ[i]);
+	while (++(data->exec_counter))
+	{
+		_print(prompt);
+		error_code = string_len = _getline(data);
 
-envi[i] = NULL;
+		if (error_code == EOF)
+		{
+			free_all_data(data);
+			exit(errno); /* if EOF is the fisrt Char of string, exit*/
+		}
+		if (string_len >= 1)
+		{
+			expand_alias(data);
+			expand_variables(data);
+			tokenize(data);
+			if (data->tokens[0])
+			{ /* if a text is given to prompt, execute */
+				error_code = execute(data);
+				if (error_code != 0)
+					_print_error(error_code, data);
+			}
+			free_recurrent_data(data);
+		}
+	}
 }
