0ad7b9b70cf02c9bb9c952ef02234f7c87d543ec
Sara <105264543+SaraFedlu@users.noreply.github.com>
2022-08-11 13:15:43 +0300

    Update memory.c

--- memory.c
+++ memory.c
@@ -1,111 +1,57 @@
 #include "shell.h"
 
 /**
- * *_realloc -  Reallocates A Memory Block Using Malloc And Free
- * @ptr: Pointer
- * @old_size: Previous Size Of The Pointer
- * @new_size: New Size Of The Pointer
- * Return: Void Pointer Rellocated Memory
+ * free_recurrent_data - free the fields needed each loop
+ * @data: struct of the program's data
+ * Return: Nothing
  */
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+void free_recurrent_data(data_of_program *data)
 {
-void *result;
+	if (data->tokens)
+		free_array_of_pointers(data->tokens);
+	if (data->input_line)
+		free(data->input_line);
+	if (data->command_name)
+		free(data->command_name);
 
-if (new_size == old_size)
-	return (ptr);
-
-if (new_size == 0 && ptr)
-{
-free(ptr);
-return (NULL);
-}
-result = malloc(new_size);
-if (result == NULL)
-	return (NULL);
-
-if (ptr == NULL)
-{
-fill_an_array(result, '\0', new_size);
-free(ptr);
-}
-else
-{
-_memcpy(result, ptr, old_size);
-free(ptr);
-}
-return (result);
-}
-/**
- * free_all - Free Array Of Char Pointer And Char Pointer
- * @cmd:Array Pointer
- * @line:Char Pointer
- * Return: Void
- */
-void free_all(char **cmd, char *line)
-{
-free(cmd);
-free(line);
-cmd = NULL;
-line = NULL;
+	data->input_line = NULL;
+	data->command_name = NULL;
+	data->tokens = NULL;
 }
 
 /**
- * _memcpy - Copy Byte From Source To Destination
- * @dest: Destination Pointer
- * @src: Source Pointer
- * @n: Size (How Much You Will Copy)
- * Return: Void Pointer
+ * free_all_data - free all field of the data
+ * @data: struct of the program's data
+ * Return: Nothing
  */
-char *_memcpy(char *dest, char *src, unsigned int n)
+void free_all_data(data_of_program *data)
 {
-unsigned int i;
+	if (data->file_descriptor != 0)
+	{
+		if (close(data->file_descriptor))
+			perror(data->program_name);
+	}
+	free_recurrent_data(data);
+	free_array_of_pointers(data->env);
+	free_array_of_pointers(data->alias_list);
+}
 
-for (i = 0; i < n; i++)
-{
-dest[i] = src[i];
-}
-return (dest);
-}
 /**
- * fill_an_array - Fill An Array By Constant Byte
- * @a:Void Pointer
- * @el: Int
- * @len:Length Int
- * Return: Void Pointer
+ * free_array_of_pointers - frees each pointer of an array of pointers and the
+ * array too
+ * @array: array of pointers
+ * Return: nothing
  */
-void *fill_an_array(void *a, int el, unsigned int len)
+void free_array_of_pointers(char **array)
 {
-char *p = a;
-unsigned int i = 0;
+	int i;
 
-while (i < len)
-{
-*p = el;
-p++;
-i++;
-}
-return (a);
-}
-/**
- * _calloc -  Allocates Memory For An Array, Using Malloc.
- * @size: Size
- * Return: Void Pointer
- */
-void *_calloc(unsigned int size)
-{
-char *a;
-unsigned int i;
+	if (array != NULL)
+	{
+		for (i = 0; array[i]; i++)
+			free(array[i]);
 
-if (size == 0)
-	return (NULL);
-
-a = malloc(size);
-if (a == NULL)
-	return (NULL);
-
-for (i = 0; i < size; i++)
-{
-a[i] = '\0';
-}
-return (a);
+		free(array);
+		array = NULL;
+	}
 }
