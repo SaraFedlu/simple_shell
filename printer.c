1602d2d20f0e856067aa5a20eaff31a977eb5891
Sara <105264543+SaraFedlu@users.noreply.github.com>
2022-08-11 13:18:21 +0300

    Update printer.c

--- printer.c
+++ printer.c
@@ -1,35 +1,70 @@
 #include "shell.h"
 
 /**
- * print_number -Print Unsigned Int Putchar
- * @n: Unisigned Integer
- * Return: Void
+ * _print - writes a array of chars in the standar output
+ * @string: pointer to the array of chars
+ * Return: the number of bytes writed or .
+ * On error, -1 is returned, and errno is set appropriately.
  */
-void print_number(unsigned int n)
+int _print(char *string)
 {
-unsigned int x = n;
-
-if ((x / 10) > 0)
-	print_number(x / 10);
-
-_putchar(x % 10 + '0');
+	return (write(STDOUT_FILENO, string, str_length(string)));
 }
 /**
- * print_number_in -Print Number Putchar
- * @n:Integer
- * Return: void
+ * _printe - writes a array of chars in the standar error
+ * @string: pointer to the array of chars
+ * Return: the number of bytes writed or .
+ * On error, -1 is returned, and errno is set appropriately.
  */
-void print_number_in(int n)
+int _printe(char *string)
 {
-unsigned int x = n;
-
-if (n < 0)
-{
-_putchar('-');
-x = -x;
+	return (write(STDERR_FILENO, string, str_length(string)));
 }
-if ((x / 10) > 0)
-	print_number(x / 10);
 
-_putchar(x % 10 + '0');
+/**
+ * _print_error - writes a array of chars in the standart error
+ * @data: a pointer to the program's data'
+ * @errorcode: error code to print
+ * Return: the number of bytes writed or .
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _print_error(int errorcode, data_of_program *data)
+{
+	char n_as_string[10] = {'\0'};
+
+	long_to_string((long) data->exec_counter, n_as_string, 10);
+
+	if (errorcode == 2 || errorcode == 3)
+	{
+		_printe(data->program_name);
+		_printe(": ");
+		_printe(n_as_string);
+		_printe(": ");
+		_printe(data->tokens[0]);
+		if (errorcode == 2)
+			_printe(": Illegal number: ");
+		else
+			_printe(": can't cd to ");
+		_printe(data->tokens[1]);
+		_printe("\n");
+	}
+	else if (errorcode == 127)
+	{
+		_printe(data->program_name);
+		_printe(": ");
+		_printe(n_as_string);
+		_printe(": ");
+		_printe(data->command_name);
+		_printe(": not found\n");
+	}
+	else if (errorcode == 126)
+	{
+		_printe(data->program_name);
+		_printe(": ");
+		_printe(n_as_string);
+		_printe(": ");
+		_printe(data->command_name);
+		_printe(": Permission denied\n");
+	}
+	return (0);
 }
