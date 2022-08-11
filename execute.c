f0c8da2f1c3c473898d8d79afd9eed64ecb5309f
Sara <105264543+SaraFedlu@users.noreply.github.com>
2022-08-11 13:09:17 +0300

    Update execute.c

--- execute.c
+++ execute.c
@@ -1,87 +1,47 @@
 #include "shell.h"
-
 /**
- * handle_builtin - Handle Builtin Command
- * @cmd: Parsed Command
- * @er:statue of last Excut
- * Return: -1 Fail 0 Succes (Return :Excute Builtin)
+ * execute - execute a command with its entire path variables.
+ * @data: a pointer to the program's data
+ * Return: If sucess returns zero, otherwise, return -1.
  */
-int handle_builtin(char **cmd, int er)
+int execute(data_of_program *data)
 {
-bul_t bil[] = {
-	{"cd", change_dir},
-	{"env", dis_env},
-	{"help", display_help},
-	{"echo", echo_bul},
-	{"history", history_dis},
-	{NULL, NULL}
-};
+	int retval = 0, status;
+	pid_t pidd;
 
-int i = 0;
+	/* check for program in built ins */
+	retval = builtins_list(data);
+	if (retval != -1)/* if program was found in built ins */
+		return (retval);
 
-while ((bil + i)->command)
-{
-if (_strcmp(cmd[0], (bil + i)->command) == 0)
-{
-return ((bil + i)->fun(cmd, er));
-}
-i++;
-}
-return (-1);
-}
-/**
- * check_cmd - Excute Simple Shell Command (Fork,Wait,Excute)
- * @cmd:Parsed Command
- * @input: User Input
- * @c:Shell Excution Time Case of Command Not Found
- * @argv:Program Name
- * Return: 1 Case Command Null -1 Wrong Command 0 Command Excuted
- */
-int check_cmd(char **cmd, char *input, int c, char **argv)
-{
-int status;
-pid_t pid;
-
-if (*cmd == NULL)
-{
-return (-1);
-}
-
-pid = fork();
-if (pid == -1)
-{
-perror("Error");
-return (-1);
-}
-if (pid == 0)
-{
-if (_strncmp(*cmd, "./", 2) != 0 && _strncmp(*cmd, "/", 1) != 0)
-{
-path_cmd(cmd);
-}
-
-if (execve(*cmd, cmd, environ) == -1)
-{
-print_error(cmd[0], c, argv);
-free(input);
-free(cmd);
-exit(EXIT_FAILURE);
-}
-return (EXIT_SUCCESS);
-}
-
-wait(&status);
-return (0);
-}
-/**
- * signal_to_handel - Handle ^C
- * @sig:Captured Signal
- * Return: Void
- */
-void signal_to_handel(int sig)
-{
-if (sig == SIGINT)
-{
-PRINTER("\n$ ");
-}
+	/* check for program file system */
+	retval = find_program(data);
+	if (retval)
+	{/* if program not found */
+		return (retval);
+	}
+	else
+	{/* if program was found */
+		pidd = fork(); /* create a child process */
+		if (pidd == -1)
+		{ /* if the fork call failed */
+			perror(data->command_name);
+			exit(EXIT_FAILURE);
+		}
+		if (pidd == 0)
+		{/* I am the child process, I execute the program*/
+			retval = execve(data->tokens[0], data->tokens, data->env);
+			if (retval == -1) /* if error when execve*/
+				perror(data->command_name), exit(EXIT_FAILURE);
+		}
+		else
+		{/* I am the father, I wait and check the exit status of the child */
+			wait(&status);
+			if (WIFEXITED(status))
+				errno = WEXITSTATUS(status);
+			else if (WIFSIGNALED(status))
+				errno = 128 + WTERMSIG(status);
+		}
+	}
+	return (0);
 }
