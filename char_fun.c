c12fc99447f4dd2d9ddc4750adc2b7fa70cc669b
Sara <105264543+SaraFedlu@users.noreply.github.com>
2022-08-11 13:19:21 +0300

    Update char_fun.c

--- char_fun.c
+++ char_fun.c
@@ -1,95 +1,154 @@
 #include "shell.h"
+
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * str_length - returns the length of a string.
+ * @string: pointer to string.
+ * Return: length of string.
  */
-int _putchar(char c)
+int str_length(char *string)
 {
-return (write(1, &c, 1));
+	int length = 0;
+
+	if (string == NULL)
+		return (0);
+
+	while (string[length++] != '\0')
+	{
+	}
+	return (--length);
 }
+
 /**
- * _strncpy - copie a string
- * @dest:char
- *  @src:char
- *  @n:int
- *  Return:char
+ * str_duplicate - duplicates an string
+ * @string: String to be copied
+ * Return: pointer to the array
  */
-char *_strncpy(char *dest, char *src, int n)
+char *str_duplicate(char *string)
 {
-int i;
+	char *result;
+	int length, i;
 
-i = 0;
+	if (string == NULL)
+		return (NULL);
 
-while (i < n && *(src + i))
-{
-*(dest + i) = *(src + i);
-i++;
+	length = str_length(string) + 1;
+
+	result = malloc(sizeof(char) * length);
+
+	if (result == NULL)
+	{
+		errno = ENOMEM;
+		perror("Error");
+		return (NULL);
+	}
+	for (i = 0; i < length ; i++)
+	{
+		result[i] = string[i];
+	}
+
+	return (result);
 }
-while (i < n)
-{
-*(dest + i) = '\0';
-i++;
-}
-return (dest);
-}
+
 /**
- * _strlen - lenght of string
- * @s:char
- * Return:int
+ * str_compare - Compare two strings
+ * @string1: String one, or the shorter
+ * @string2: String two, or the longer
+ * @number: number of characters to be compared, 0 if infinite
+ * Return: 1 if the strings are equals,0 if the strings are different
  */
-int _strlen(char *s)
+int str_compare(char *string1, char *string2, int number)
 {
-int i;
+	int iterator;
 
-for (i = 0; s[i] != '\0'; i++)
-{
-continue;
+	if (string1 == NULL && string2 == NULL)
+		return (1);
+
+	if (string1 == NULL || string2 == NULL)
+		return (0);
+
+	if (number == 0) /* infinite longitud */
+	{
+		if (str_length(string1) != str_length(string2))
+			return (0);
+		for (iterator = 0; string1[iterator]; iterator++)
+		{
+			if (string1[iterator] != string2[iterator])
+				return (0);
+		}
+		return (1);
+	}
+	else /* if there is a number of chars to be compared */
+	{
+		for (iterator = 0; iterator < number ; iterator++)
+		{
+			if (string1[iterator] != string2[iterator])
+			return (0);
+		}
+		return (1);
+	}
 }
-return (i);
-}
+
 /**
- * _atoi - convert to a int
- * @s:string
- * Return:int
+ * str_concat - concatenates two strings.
+ * @string1: String to be concatenated
+ * @string2: String to be concatenated
+ *
+ * Return: pointer to the array
  */
-int _atoi(char *s)
+char *str_concat(char *string1, char *string2)
 {
-int i, j, n, x;
+	char *result;
+	int length1 = 0, length2 = 0;
 
-i = n = 0;
-x = 1;
+	if (string1 == NULL)
+		string1 = "";
+	length1 = str_length(string1);
 
-while ((s[i] < '0' || s[i] > '9') && (s[i] != '\0'))
-{
-if (s[i] == '-')
-	x *= -1;
+	if (string2 == NULL)
+		string2 = "";
+	length2 = str_length(string2);
 
-i++;
+	result = malloc(sizeof(char) * (length1 + length2 + 1));
+	if (result == NULL)
+	{
+		errno = ENOMEM;
+		perror("Error");
+		return (NULL);
+	}
+
+	/* copy of string1 */
+	for (length1 = 0; string1[length1] != '\0'; length1++)
+		result[length1] = string1[length1];
+	free(string1);
+
+	/* copy of string2 */
+	for (length2 = 0; string2[length2] != '\0'; length2++)
+	{
+		result[length1] = string2[length2];
+		length1++;
+	}
+
+	result[length1] = '\0';
+	return (result);
 }
-j = i;
 
-while ((s[j] >= '0') && (s[j] <= '9'))
-{
-n = (n * 10) + x*((s[j]) - '0');
-j++;
-}
-return (n);
-}
+
 /**
- * _puts - print a string
- * @str:pointer char
- * return:void
+ * str_reverse - reverses a string.
+ *
+ * @string: pointer to string.
+ * Return: void.
  */
-void _puts(char *str)
+void str_reverse(char *string)
 {
-int i;
 
-for (i = 0; str[i] != '\0'; i++)
-{
-_putchar(str[i]);
-}
-_putchar('\n');
-return;
+	int i = 0, length = str_length(string) - 1;
+	char hold;
+
+	while (i < length)
+	{
+		hold = string[i];
+		string[i++] = string[length];
+		string[length--] = hold;
+	}
 }
